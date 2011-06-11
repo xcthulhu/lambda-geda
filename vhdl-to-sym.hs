4031663961f9e9f7b043ad30249faf36c9a2c3cd
Matthew Wampler-Doty <negacthulhu@gmail.com>
2011-06-11 12:36:40 -0600

    Making arrows now

--- vhdl-to-sym.hs
+++ vhdl-to-sym.hs
@@ -18,53 +18,114 @@ len = 300
 spacing :: Int
 spacing = 400
 
+-- Size for text
+text_size :: Int
+text_size = 10
+
 -- Draw a single pin
-dpin whichend x y port = let
+dpin ((x1,y1),(x2,y2)) port = let
   (ident, dir, typ, val) = port
-  (x1,y1,x2,y2) = case dir of 
-    IN -> (x,y,x+len,y)
-    OUT -> (x+len,y,x,y)
-    INOUT -> (x,y,x,y+len)
   color = 1
-  pintype = 0
-  atts = []
-  in P {..}
+  arr = L { line_width = 0, capstyle = 0, dashstyle = 0,
+            dashlength = -1, dashspace = -1, atts = [], 
+            .. }
+  -- Draw arrows
+  (arrowp1,arrowp2) = case dir of
+    IN -> 
+      (arr{x2=round $ toRational (7*x1-x2+y1-y2) / 6,
+           y2=round $ toRational (-x1+x2+7*y1-y2) / 6},
+       arr{x2=round $ toRational (7*x1-x2-y1+y2) / 6,
+           y2=round $ toRational (x1-x2+7*y1-y2) / 6})
+    OUT -> 
+      (arr{x2=round $ toRational (5*x1+x2+y1-y2) / 6,
+           y2=round $ toRational (-x1+x2+5*y1+y2) / 6},
+       arr{x2=round $ toRational (5*x1+x2-y1+y2) / 6,
+           y2=round $ toRational (x1-x2+5*y1+y2) / 6})
+    INOUT -> 
+      (arr{x1=x2, y1=y2,
+           x2=round $ toRational (x1+5*x2+y1-y2) / 6,
+           y2=round $ toRational (-x1+x2+y1+5*y2)/ 6},
+       arr{x1=x2, y1=y2,
+           x2=round $ toRational (x1+5*x2-y1+y2) / 6,
+           y2=round $ toRational (x1-x2+y1+5*y2) / 6})      
+  in [P {pintype=0, whichend = 0, atts=[], ..}, arrowp1, arrowp2]
+
+-- Calculates the offset from input parameters
+offp offset d n = offset + (max 0 $ round $ toRational
+                         (d - ((n+1) * spacing)) / 2)
 
 -- Draw several horizontal pins parallel to one another, 
 -- forming a verticle line
 vlpins whichend hoffset voffset vheight ports = 
-  let voffsetp = 
-        voffset + (max 0 $ round $ toRational 
-                   (vheight - length ports * spacing) / 2)
-  in zipWith (dpin whichend hoffset) 
-             [voffsetp + i*spacing | i <- [1..length ports]]
-             ports
+  let voffsetp = offp voffset vheight (length ports)
+      (x1,x2) = if whichend == 0 
+                 then (hoffset, hoffset + len)
+                 else (hoffset + len, hoffset)
+      coords = do i <- [1..length ports]
+                  let y = voffsetp + i*spacing
+                  return ((x1,y),(x2,y))
+  in concat $ zipWith dpin coords ports
      
 -- Draw several verticle pins parallel to one another, 
 -- forming a horizontal line
 hlpins whichend hoffset voffset hwidth ports = 
-  let hoffsetp = 
-        hoffset + (max 0 $ round $ toRational
-                   (hwidth - length ports * spacing) / 2)
-  in zipWith (\x -> dpin whichend x voffset)
-             [hoffsetp + i*spacing | i <- [1..length ports]]
-             ports
+  let hoffsetp = offp hoffset hwidth (length ports)
+      (y1,y2) = if whichend == 0 
+                 then (voffset, voffset + len)
+                 else (voffset + len, voffset)
+      coords = do i <- [1..length ports]
+                  let x = hoffsetp + i*spacing
+                  return ((x,y1),(x,y2))
+  in concat $ zipWith dpin coords ports
 
---entity2schematic :: Entity -> [GSchem]
+-- Calculates the width from an input string
+calcWidth s = (length s) * 10 * text_size + len + (length "U?") * 10 * text_size
+
+-- Draw a box with component name in the center
+mkbox x y hwidth vheight devname =
+  let size = text_size
+      width = max hwidth (calcWidth devname)
+      path = [ MM [(x,y)]
+             , Ll [ (0, vheight) 
+                  , (width, 0)
+                  , (0,-vheight) ]
+             , Z ]
+      angle = 0
+      atts = [] in
+  [ H { color = 3, line_width = 0, capstyle = 0, 
+        dashstyle = 0, dashlength = -1, dashspace = -1, 
+        filltype = 0, fillwidth = -1, angle1 = -1, 
+        pitch1 = -1, angle2 = -1, pitch2 = -1, 
+        num_lines = length path, .. }
+  , T { x1 = x + width, y1 = y + vheight + 15, color = 8, 
+        visibility = 1, show_name_value = 1, alignment = 6, 
+        num_lines = 1, text = ["refdes=U?"], .. }
+  , T { x1 = x, y1 = y + vheight, color = 5, 
+        visibility = 0, show_name_value = 0, alignment = 0, 
+        num_lines = 1, text = ["device=" ++ devname], .. }
+  , T { x1 = round (toRational (2*x + width) / 2), 
+        y1 = round (toRational (2*y + vheight) / 2), 
+        color = 9, visibility = 1, show_name_value = 0, 
+        alignment = 4, num_lines = 1, 
+        text = [devname], .. } ]
+
+entity2schematic :: FilePath -> Entity -> [GSchem]
 entity2schematic out_dir entity = 
-  [ Basename (identifier entity),
-    Dirname (out_dir),
-    Version 20110115 2 ] ++
-  (vlpins 0 0 0 vheight inpins) ++
-  (vlpins 0 (hoffset + hwidth)  0 vheight outpins) ++
-  (hlpins 0 hoffset vheight hwidth iopins)
+  [ Basename (identifier entity ++ ".sym")
+  , Dirname out_dir
+  , Version 20110115 2 ]
+  ++ mkbox len len hwidth vheight (identifier entity)
+  ++ vlpins 0 0 voffset vheight inpins
+  ++ vlpins 1 (hoffset + hwidth) voffset vheight outpins
   where
     inpins = filter (\(_,dir,_,_) -> dir == IN) $ port entity
-    outpins = filter (\(_,dir,_,_) -> dir == OUT) $ port entity
-    iopins = filter (\(_,dir,_,_) -> dir == INOUT) $ port entity
+    outpins = filter (\(_,dir,_,_) -> dir == OUT || 
+                                      dir == INOUT) $ port entity
+    voffset = len
     hoffset = len
-    vheight = spacing * max (length inpins) (length outpins)
-    hwidth = spacing * length iopins
+    vheight = spacing * (1 + max (length inpins) 
+                                 (length outpins))
+    hwidth = calcWidth $ identifier entity
 
 -- Process an entity to a schematic in the IO monad
 proc out_dir in_fns = do
@@ -72,11 +133,9 @@ proc out_dir in_fns = do
     then return [stdin]
     else mapM (`openFile` ReadMode) in_fns
   cnts <- mapM hGetContents in_fhs
-  let ents =  
-             map ((\(Right x) -> x) . readEntity) $ 
+  let ents = map ((\(Right x) -> x) . readEntity) $ 
              concat $ map entityStrings cnts
-  --mapM_ (fnPutGSchematic "-") $
-    mapM_ (putStr.show) $
+  mapM_ (fnPutGSchematic "_") $
          map (entity2schematic out_dir) ents
   if (in_fns == ["-"] || in_fns == []) 
     then return () 
@@ -87,8 +146,10 @@ main = do
   -- Parse command line arguments to determine input/output behavior
   pn <- getProgName
   args <- getArgs
-  if (any (=="--help") args || args == []) then
-    putStrLn ("Usage: " ++ pn ++ "[OUTPUTDIRECTORY] [[INPUT1] [INPUT2] [INPUT3] ...]") >> exitSuccess
+  if (any (=="--help") args || args == []) 
+    then putStrLn ("Usage: " ++ pn ++ 
+                   "[OUTPUTDIRECTORY] [[INPUT1] [INPUT2] [INPUT3] ...]") 
+      >> exitSuccess
     else return ()
   proc (head args) (tail args)
   exitSuccess
